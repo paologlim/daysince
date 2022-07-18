c6c244cc7607dea18f66eab72647d8c2ed3a49e6
Paolo Granada Lim <paologlim@gmail.com>
2022-07-18 16:44:22 +0800

    Add initial interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Days Since',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
       home: const MyHomePage(title: 'Days Since'),
     );
@@ -30,22 +31,58 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  SharedPreferences? prefs;
+  List<Map<String, dynamic>>? habits;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'Hello World',
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-    );
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () => null,
+        ),
+        body: ListView(
+          children: [
+            ListTile(
+                title: Text(_buildTimeSince('2022-07-17')),
+                subtitle: const Text('Drank alcohol'),
+                trailing: IconButton(
+                    onPressed: () => null, icon: const Icon(Icons.replay)),
+                leading: const Icon(Icons.mood)),
+            ListTile(
+                title: const Text('7 days 10 hours 20 minutes 9 seconds ago'),
+                subtitle: const Text('Took a walk'),
+                trailing: IconButton(
+                    onPressed: () => null, icon: const Icon(Icons.replay)),
+                leading: const Icon(Icons.mood_bad))
+          ],
+        ));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initHabits();
+  }
+
+  void _initHabits() async {
+    prefs = await SharedPreferences.getInstance();
+    habits = prefs
+        ?.getStringList('habits')
+        ?.map((String e) {
+          List<String> row = e.split(',');
+          return {'title': row[0], 'date': row[1]};
+        })
+        .cast<Map<String, dynamic>>()
+        .toList();
+  }
+
+  String _buildTimeSince(String date) {
+    Duration dateDiff = DateTime.now().difference(DateTime.parse(date));
+    return '${dateDiff.inDays} days ${dateDiff.inHours % 24} hours '
+        '${dateDiff.inMinutes % 60} minutes ${dateDiff.inSeconds % 60} seconds';
   }
 }
